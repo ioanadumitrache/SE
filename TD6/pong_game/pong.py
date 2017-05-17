693d84e42804dbb487c790e255d29a0c22d50801
ioanadumitrache <ioana.dumitrache@asaff.ro>
2017-05-17 23:27:32 +0300

    Update pong.py

--- TD6/pong_game/pong.py
+++ TD6/pong_game/pong.py
@@ -1,6 +1,7 @@
-from PyQt4.QtCore import *
-from PyQt4.QtGui import *
-from PyQt4.QtOpenGL import QGLWidget
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtOpenGL import *
 import sys
 import random
 
